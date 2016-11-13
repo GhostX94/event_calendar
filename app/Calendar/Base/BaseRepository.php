2ea10ab1afe10006fce6b10f22a274b1ac6edbd5
ajpadilla <ajpadilla88@gmail.com>
2016-11-12 20:12:20 -0400

    Agregando clase repository de la cual heredaran todos los repositorios

---
+++ app/Calendar/Base/BaseRepository.php
@@ -0,0 +1,51 @@
+<?php  
+	
+	namespace App\Calendar\Base;
+	
+	class BaseRepository
+	{
+		protected $model;
+	  
+	    public function setModel($model)
+	    {
+	    	$this->model = $model;
+	    }
+
+	    public function getModel()
+		{
+			return $this->model;
+		}
+
+		public function create($data = array())
+		{
+			$model = $this->model->create($data); 
+			return $model;
+		}
+
+		public function  getAll($exclude = null)
+		{
+			if($exclude)
+				return $this->getModel()->whereNotIn('id', $exclude)->get();
+			return $this->getModel()->all();
+		}
+
+		public function getAllForSelect()
+		{
+			return $this->getAll()->lists('name', 'id');
+		}
+
+		public function get($id)
+		{
+			return $this->model->findOrFail($id);
+		}
+
+		public function delete($id)
+		{
+			$model = $this->get($id); 
+			return $model->delete();
+		}
+		
+		public function update($data = array()){}
+	}
+
+?>
