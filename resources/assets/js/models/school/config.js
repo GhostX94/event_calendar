9d2127f31348a71f86baf0163f82cb8b68de2ef1
ajpadilla <ajpadilla88@gmail.com>
2016-11-15 22:50:24 -0400

    actualizando archivo de configuracion

--- resources/assets/js/models/school/config.js
+++ resources/assets/js/models/school/config.js
@@ -11,32 +11,6 @@
                 sortField: 'name',
             },
             {
-                name: 'email',
-                sortField: 'email',
-            },
-            {
-                name: 'nickname',
-                sortField: 'nickname',
-                callback: 'allCap'
-            },
-            {
-                name: 'birthdate',
-                sortField: 'birthdate',
-                //callback: 'formatDate|D/MM/Y'
-            },
-            {
-                name: 'gender',
-                sortField: 'gender',
-                titleClass: 'text-center',
-                dataClass: 'text-center',
-                callback: 'gender'
-            },
-            {
-                name: 'address',
-                sortField: 'address',
-                callback: 'address',
-            },
-            {
                 name: '__component:custom-action',
                 title: "Component",
                 titleClass: 'center aligned',
