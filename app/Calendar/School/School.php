46f92fc700ce08f0a74edb6d81a16793f2869baf
ajpadilla <ajpadilla88@gmail.com>
2016-11-15 21:56:44 -0400

    Agregando trait a la clase

--- app/Calendar/School/School.php
+++ app/Calendar/School/School.php
@@ -3,9 +3,12 @@
 namespace App\Calendar\School;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Calendar\SearchTrait;
+use App\Calendar\SortTrait;
 
 class School extends Model
 {
+	use SearchTrait, SortTrait;
 
 	public $table = 'schools';
 	protected $fillable = ['name'];
