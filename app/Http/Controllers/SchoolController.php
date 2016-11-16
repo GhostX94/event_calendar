56e6a940547f5c6de8ba0f7c2acdbeeae38cce3d
ajpadilla <ajpadilla88@gmail.com>
2016-11-15 22:49:47 -0400

    Reescribiendo metodo index

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 
 use App\Calendar\School\SchoolRepository;
+use App\Calendar\School\School;
 
 class SchoolController extends Controller
 {
@@ -18,7 +19,25 @@ class SchoolController extends Controller
 
 	public function index(Request $request)
 	{
-		return response()->json(['data'=> request()->all()]);
+		if (request()->has('sort')) 
+		{
+			list($sortCol, $sortDir) = explode('|', request()->sort);
+			if(\Schema::hasColumn('schools', $sortCol))
+				$query = School::orderBy($sortCol, $sortDir);
+			else
+				$query = School::sortBy($sortCol, $sortDir);
+		}else{
+			$query = School::orderBy('created_at', 'asc');
+		}
+
+		if ($request->exists('filter')) {
+			$query->search("{$request->filter}");
+		}
+
+		$perPage = request()->has('per_page') ? (int) request()->per_page : null;
+		$result = $query->paginate($perPage);
+
+		return response()->json($result);
 	}
 
 }
