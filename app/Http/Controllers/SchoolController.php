bf7cea5a94f81368363f14802aa995d8bee8910e
ajpadilla <ajpadilla88@gmail.com>
2016-11-15 21:16:28 -0400

    Definiendo metodo index

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -6,22 +6,19 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-use Illuminate\Http\Request;
-
 use App\Calendar\School\SchoolRepository;
-
-private $repository;
 
 class SchoolController extends Controller
 {
-   public function __construct(SchoolRepository $schoolRepository)
-   {
+	private $repository;
+	public function __construct(SchoolRepository $schoolRepository)
+	{
 		$this->repository = $schoolRepository;
-   }
+	}
 
-   public function index(Request $request)
-   {
- 		
-   }
+	public function index(Request $request)
+	{
+		return response()->json(['data'=> request()->all()]);
+	}
 
 }
