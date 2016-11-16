007e497b505643c18f8c61eb4e372e04161f8bcb
ajpadilla <ajpadilla88@gmail.com>
2016-11-15 22:50:49 -0400

    actualizando archivo de configuracion

--- resources/views/admin/schools/table.blade.php
+++ resources/views/admin/schools/table.blade.php
@@ -1,17 +1,17 @@
 <div class="row">
-            <div class="col-md-7 form-inline">
-                <div class="form-inline form-group">
-                    <label>Search:</label>
-                    <input class="form-control">
-                    <button class="btn btn-primary">Go</button>
-                    <button class="btn btn-default">Reset</button>
-                </div>
-            </div>
-            <div class="col-md-5">
-                <div class="form-inline form-group pull-right">
-                    <button class="btn btn-default" data-toggle="modal" data-target="#settingsModal">
-                        <span class="glyphicon glyphicon-cog"></span> Settings
-                    </button>
-                </div>
-            </div>
-        </div>
+    <div class="col-md-7 form-inline">
+        <div class="form-inline form-group">
+          <label>Search:</label>
+          <input v-model="searchFor" class="form-control" @keyup.enter="setFilter">
+          <button class="btn btn-primary" @click="setFilter">Go</button>
+          <button class="btn btn-default" @click="resetFilter">Reset</button>
+      </div>
+  </div>
+  <div class="col-md-5">
+    <div class="form-inline form-group pull-right">
+        <button class="btn btn-default" data-toggle="modal" data-target="#settingsModal">
+            <span class="glyphicon glyphicon-cog"></span> Settings
+        </button>
+    </div>
+</div>
+</div>
