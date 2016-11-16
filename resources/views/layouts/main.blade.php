0385c1e842cbf882c11be886567f8cc1740c36b4
ajpadilla <ajpadilla88@gmail.com>
2016-11-15 21:13:21 -0400

    Agregando archivos .js y .css generales

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,10 +4,13 @@
 	<meta charset="UTF-8">
 	<title>@yield('title')</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+  	{!! Html::style('css/app.min.css') !!}
 	@stack('styles')
 </head>
 <body>
 	@yield('main-content')
+
+	{!! Html::script('js/app.min.js') !!}
 	@stack("scripts")
 </body>
 </html>
