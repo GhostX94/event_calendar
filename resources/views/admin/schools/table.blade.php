99a96579665c48145fdaa9f8212db346da3bbafb
ajpadilla <ajpadilla88@gmail.com>
2016-11-15 21:08:27 -0400

    Agragando blade para listar items del modelo school

---
+++ resources/views/admin/schools/table.blade.php
@@ -0,0 +1,17 @@
+<div class="row">
+            <div class="col-md-7 form-inline">
+                <div class="form-inline form-group">
+                    <label>Search:</label>
+                    <input class="form-control">
+                    <button class="btn btn-primary">Go</button>
+                    <button class="btn btn-default">Reset</button>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="form-inline form-group pull-right">
+                    <button class="btn btn-default" data-toggle="modal" data-target="#settingsModal">
+                        <span class="glyphicon glyphicon-cog"></span> Settings
+                    </button>
+                </div>
+            </div>
+        </div>
