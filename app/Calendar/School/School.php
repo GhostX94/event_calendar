8be4daa2b08d96123e5efa65d573de6972f6c92c
ajpadilla <ajpadilla88@gmail.com>
2016-12-09 22:27:03 -0400

    Comentando atributos de la clase

--- app/Calendar/School/School.php
+++ app/Calendar/School/School.php
@@ -10,10 +10,25 @@ class School extends Model
 {
 	use SearchTrait, SortTrait;
 
+	/**
+     * The table associated with the model.
+     *
+     * @var string
+    */
 	public $table = 'schools';
 
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+    */
 	protected $fillable = ['name'];
 
+	/**
+     * 	The columns used for searches in each query.
+     *
+     * @var array
+    */
 	protected $searchableColumns = [
 		'name'
 	];
