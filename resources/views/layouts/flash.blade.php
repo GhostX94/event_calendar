551e2e494dbf2bbd1f0944918b5e81b9bd49a3ae
ajpadilla <ajpadilla88@gmail.com>
2016-12-12 14:38:03 -0400

    Listado errores devueltos por el servidor

--- resources/views/layouts/flash.blade.php
+++ resources/views/layouts/flash.blade.php
@@ -1,5 +1,8 @@
 <div v-if="flashMessage" class="alert alert-@{{ flashType }}">
 	<b>@{{ flashMessage }}</b>
+	<ul class="list-group" v-if="errorMessages">
+	  <li class="list-group-item" v-for="error in errorMessages">@{{ error }}</li>
+	</ul>
 	<hr v-if="$validation.errors">
 	<validator-errors :component="'custom-error'" :validation="$validation"></validator-errors>
 </div>
