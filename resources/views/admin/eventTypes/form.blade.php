9b9495ad925a525b64bbbd6399d6287ea6628c1f
ajpadilla <ajpadilla88@gmail.com>
2016-12-12 17:32:11 -0400

    Modificando ruta de archivo

--- resources/views/admin/eventTypes/form.blade.php
+++ resources/views/admin/eventTypes/form.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.modal.form')
 
 @section('modal-form-title')
-Form Schools
+Form Event Type
 @stop
 
 @section('modal-form-content')
-	@include('admin.schools.fields')
+	@include('admin.eventTypes.fields')
 @stop
