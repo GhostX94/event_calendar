068279bfac506af24a77d92c94e30c3d2f966cd7
ajpadilla <ajpadilla88@gmail.com>
2016-12-12 15:26:04 -0400

    Definiendo atributos del modelo

--- app/Calendar/EventType/EventType.php
+++ app/Calendar/EventType/EventType.php
@@ -8,5 +8,30 @@ use App\Calendar\SortTrait;
 
 class EventType extends Model
 {
-    //
+	use SearchTrait, SortTrait;
+
+	/**
+     * The table associated with the model.
+     *
+     * @var string
+    */
+	public $table = 'schools';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+    */
+	protected $fillable = ['name'];
+
+	/**
+     * 	The columns used for searches in each query.
+     *
+     * @var array
+    */
+	protected $searchableColumns = [
+		'name'
+	];
+
+
 }
