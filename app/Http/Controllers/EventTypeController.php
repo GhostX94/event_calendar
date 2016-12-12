8660bc58b5e35523deedc6fe44ebfcb5982034a4
ajpadilla <ajpadilla88@gmail.com>
2016-12-12 15:40:39 -0400

    Agregando metodo index

--- app/Http/Controllers/EventTypeController.php
+++ app/Http/Controllers/EventTypeController.php
@@ -16,4 +16,30 @@ class EventTypeController extends Controller
     {
    		$this->repository = $eventTypeRepository;
     }
+
+    public function index(Request $request)
+	{
+		if (request()->ajax()) 
+		{
+			if (request()->has('sort')) 
+			{
+				list($sortCol, $sortDir) = explode('|', request()->sort);
+				if(\Schema::hasColumn('schools', $sortCol))
+					$query = School::orderBy($sortCol, $sortDir);
+				else
+					$query = School::sortBy($sortCol, $sortDir);
+			}else{
+				$query = School::orderBy('created_at', 'asc');
+			}
+
+			if ($request->exists('filter')) {
+				$query->search("{$request->filter}");
+			}
+
+			$perPage = request()->has('per_page') ? (int) request()->per_page : null;
+			$result = $query->paginate($perPage);
+
+			return response()->json($result);
+		}	
+	}
 }
