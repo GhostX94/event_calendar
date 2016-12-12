c5227ad10f75091c79bbe76a1ee6fb432e3cd59b
ajpadilla <ajpadilla88@gmail.com>
2016-12-12 15:40:17 -0400

    Definiendo constructor de la clase

--- app/Http/Controllers/EventTypeController.php
+++ app/Http/Controllers/EventTypeController.php
@@ -6,7 +6,14 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Calendar\EventType\EventTypeRepository;
+
 class EventTypeController extends Controller
 {
-    //
+    private $repository; 
+
+    public function __construct(EventTypeRepository $eventTypeRepository)
+    {
+   		$this->repository = $eventTypeRepository;
+    }
 }
