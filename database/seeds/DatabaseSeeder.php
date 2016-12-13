296a01bf7b386ffb18ffecb89b806be83dbeef33
ajpadilla <ajpadilla88@gmail.com>
2016-12-13 13:35:31 -0400

    Llamando a clase SchoolLevelTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(SchoolTableSeeder::class);
         $this->call(EventTypeTableSeeder::class);
+        $this->call(SchoolLevelTableSeeder::class);
     }
 }
