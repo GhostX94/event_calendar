b61f06cc72f47702e7766d600e9bd0b0e72f2196
ajpadilla <ajpadilla88@gmail.com>
2016-12-13 14:32:55 -0400

    Agregando metodo delete del controlador

--- app/Http/Controllers/SchoolLevelController.php
+++ app/Http/Controllers/SchoolLevelController.php
@@ -89,4 +89,15 @@ class SchoolLevelController extends Controller
 		}
 	}
 
+	public function destroy($id, Request $request)
+	{
+		if (request()->ajax()) 
+		{
+			$input = $request->all();
+			$this->setSuccess($this->repository->delete($id));
+			$this->addToResponseArray('message', 'School Level delete');
+			return $this->getResponseArrayJson(); 
+		}
+	}
+
 }
