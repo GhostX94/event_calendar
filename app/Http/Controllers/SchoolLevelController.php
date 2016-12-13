5228e4fd40ea63537b30b5475a20ecb7d8ff869d
ajpadilla <ajpadilla88@gmail.com>
2016-12-13 13:23:59 -0400

    Definiendo metodo index del controlador

---
+++ app/Http/Controllers/SchoolLevelController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Calendar\SchoolLevel\SchoolLevel;
+use App\Calendar\SchoolLevel\SchoolLevelRepository;
+
+class SchoolLevelController extends Controller
+{
+	private $repository;
+
+    public function __construct(SchoolLevelRepository $schoolLevelRepository)
+    {
+    	$this->repository = $schoolLevelRepository;
+    }
+
+    public function index(Request $request)
+	{
+		if (request()->ajax()) 
+		{
+			if (request()->has('sort')) 
+			{
+				list($sortCol, $sortDir) = explode('|', request()->sort);
+				if(\Schema::hasColumn('schools', $sortCol))
+					$query = SchoolLevel::orderBy($sortCol, $sortDir);
+				else
+					$query = SchoolLevel::sortBy($sortCol, $sortDir);
+			}else{
+				$query = SchoolLevel::orderBy('created_at', 'asc');
+			}
+
+			if ($request->exists('filter')) {
+				$query->search("{$request->filter}");
+			}
+
+			$perPage = request()->has('per_page') ? (int) request()->per_page : null;
+			$result = $query->paginate($perPage);
+
+			return response()->json($result);
+		}	
+	}
+
+}
