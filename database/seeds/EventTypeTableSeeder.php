ff48bd4e62bbdae6b91870a86c7d2bb8eb4ccb11
ajpadilla <ajpadilla88@gmail.com>
2016-12-15 17:56:36 -0400

    Actualizando datos de prueba

--- database/seeds/EventTypeTableSeeder.php
+++ database/seeds/EventTypeTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Calendar\EventType\EventType;
 
 class EventTypeTableSeeder extends Seeder
 {
@@ -11,6 +12,15 @@ class EventTypeTableSeeder extends Seeder
      */
     public function run()
     {
-   		factory(App\Calendar\EventType\EventType::class, 3)->create();
+    	$names = [
+        	'Informativo',
+        	'Escolar',
+        	'Caridad',
+    	];
+
+    	for ($i=0; $i < count($names) ; $i++) { 
+    		EventType::create(['name' => $names[$i]]);
+    	}
+   		//factory(App\Calendar\EventType\EventType::class, 3)->create();
     }
 }
