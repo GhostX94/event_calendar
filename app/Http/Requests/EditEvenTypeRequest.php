381c2c5b77698f3f1355a51230c4283996a1a745
ajpadilla <ajpadilla88@gmail.com>
2016-12-29 22:47:33 -0400

    Actualizando clase

--- app/Http/Requests/EditEvenTypeRequest.php
+++ app/Http/Requests/EditEvenTypeRequest.php
@@ -33,7 +33,7 @@ class EditEvenTypeRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|min:1|max:255|unique:event_types,name,'.$this->route->getParameter('id')
+            'name' => 'required|min:1|max:255|unique:event_types,name,'.$this->route->getParameter('id'),
         ];
     }
 }
