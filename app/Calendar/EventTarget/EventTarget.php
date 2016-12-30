df42ba1da5a33564d473d49e1eff171b9c4261ec
ajpadilla <ajpadilla88@gmail.com>
2016-12-29 21:19:38 -0400

    Actualizando clase

--- app/Calendar/EventTarget/EventTarget.php
+++ app/Calendar/EventTarget/EventTarget.php
@@ -10,4 +10,27 @@ class EventTarget extends Model
 {
 	use SearchTrait, SortTrait;
 
+	/**
+     * The table associated with the model.
+     *
+     * @var string
+    */
+	public $table = 'event_targets';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+    */
+	protected $fillable = ['name'];
+
+	/**
+     * 	The columns used for searches in each query.
+     *
+     * @var array
+    */
+	protected $searchableColumns = [
+		'name'
+	];
+
 }
