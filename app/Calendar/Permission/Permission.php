ceeec9685cce575c593c2442c1f02947da06ed1a
ajpadilla <ajpadilla88@gmail.com>
2017-01-02 22:08:09 -0400

    Agregando clase para manipular permisos de acceso al sistema

---
+++ app/Calendar/Permission/Permission.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Calendar\Permission;
+
+use Illuminate\Database\Eloquent\Model;
+use Zizaco\Entrust\EntrustPermission;
+
+class Permission extends EntrustPermission
+{
+    //
+}
