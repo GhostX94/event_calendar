30c960d157268238684146b938b909183cc4fb70
ajpadilla <ajpadilla88@gmail.com>
2017-01-02 22:41:04 -0400

    Modificando carga de usuarios de prueba

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Calendar\User\User;
 
 class UserTableSeeder extends Seeder
 {
@@ -11,6 +12,26 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-		factory(App\Calendar\User\User::class, 10)->create();
+		//factory(App\Calendar\User\User::class, 10)->create();
+		$users = [];
+
+		$users [] = [
+	        'name' => 'ajpadilla88',
+	        'email' => 'ajpadilla88@gmail.com',
+	        'password' => $password ?: $password = bcrypt('secret'),
+	        'remember_token' => str_random(10),
+    	];
+
+    	$users [] = [
+	        'name' => 'tego_07_07',
+	        'email' => 'tego_07_07@hotmail.com',
+	        'password' => $password ?: $password = bcrypt('secret'),
+	        'remember_token' => str_random(10),
+    	];
+
+    	foreach ($users as $user) {
+    		User::create($user);
+    	}
+
     }
 }
