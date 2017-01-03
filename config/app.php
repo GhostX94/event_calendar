72444718fd0787ec5be6b751057dd538ffe1b066
ajpadilla <ajpadilla88@gmail.com>
2017-01-02 20:26:17 -0400

    Agregando paquete para majenar roles

--- config/app.php
+++ config/app.php
@@ -169,6 +169,8 @@ return [
 
         Collective\Html\HtmlServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Zizaco\Entrust\EntrustServiceProvider::class,
+
 
         //
 
@@ -230,7 +232,9 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
+
     ],
 
 ];
