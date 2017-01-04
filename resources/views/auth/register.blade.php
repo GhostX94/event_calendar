7f2ac42623801c6d30046da12cf9ca4288e02e74
ajpadilla <ajpadilla88@gmail.com>
2017-01-03 22:29:00 -0400

    Agregando nuevos campos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,10 +11,10 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-4 control-label">Username</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -28,7 +28,7 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -38,11 +38,115 @@
                             </div>
                         </div>
 
+                         <div class="form-group{{ $errors->has('firts_name') ? ' has-error' : '' }}">
+                            <label for="firts_name" class="col-md-4 control-label">First Name</label>
+
+                            <div class="col-md-6">
+                                <input id="firts_name" type="text" class="form-control" name="firts_name" value="{{ old('firts_name') }}">
+
+                                @if ($errors->has('firts_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('firts_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
+                            <label for="last_name" class="col-md-4 control-label">Last Name</label>
+
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
+
+                                @if ($errors->has('last_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('last_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('birthdate') ? ' has-error' : '' }}">
+                            <label for="birthdate" class="col-md-4 control-label">Birthdate</label>
+
+                            <div class="col-md-6">
+                                <input id="birthdate" type="text" class="form-control" name="birthdate" value="{{ old('birthdate') }}">
+
+                                @if ($errors->has('birthdate'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('birthdate') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                            <label for="address" class="col-md-4 control-label">Address</label>
+
+                            <div class="col-md-6">
+                                <textarea id="address" class="form-control" name="address" rows="4" cols="50">
+                                </textarea>
+                                @if ($errors->has('address'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            <label for="phone" class="col-md-4 control-label">Phone</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}">
+
+                                @if ($errors->has('phone'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('type_person_id') ? ' has-error' : '' }}">
+                            <label for="type_person_id" class="col-md-4 control-label">Type person</label>
+
+                            <div class="col-md-6">
+                                <select name="type_person_id" id="type_person_id" class="form-control">
+                                    <option value="" disabled selected>Select your option</option>
+
+                                </select>
+                                @if ($errors->has('type_person_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('type_person_id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('town_id') ? ' has-error' : '' }}">
+                            <label for="town_id" class="col-md-4 control-label">Town</label>
+
+                            <div class="col-md-6">
+                                <select name="town_id" id="town_id" class="form-control">
+                                    <option value="" disabled selected>Select your option</option>
+
+                                </select>
+                                @if ($errors->has('town_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('town_id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -56,9 +160,18 @@
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
                             </div>
                         </div>
+
+                         <div class="form-group">
+                            <label for="photo" class="col-md-4 control-label">Photo</label>
+
+                            <div class="col-md-6">
+                                <input type="file" name="photo" accept="image/*">
+                            </div>
+                        </div>
+
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
